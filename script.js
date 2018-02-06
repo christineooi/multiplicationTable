c4a93ab597bd872a96b034e89e4bd057a474c7f0
Christine Ooi <christine.ooi@gmail.com>
2018-02-05 19:46:41 -0500

    modified js file

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
 
-var color_td;
-var newHTML = "";
+var color_td = "#fff";
+var newHTML = "<tr style='height:40px;'><td style='width:40px;background-color:#ccc'>X</td> ";
+for(var x = 0; x <=10; x++){
+	newHTML += "<td style='width:40px;background-color:#ccc'>" + x + "</td>";
+}
+newHTML += "</tr>"
+
+
 for(var i = 0; i <= 10; i++) {
     newHTML += "<tr style='height:40px;'>";
-    newHTML += "<td style='width:30px;background-color:#ccc'>" + i + "</td>";
+    newHTML += "<td style='width:40px;background-color:#ccc'>" + i + "</td>";
 	for(var j = 0; j <= 10; j++) {
 		newHTML += "<td style='width:40px;background-color:" + color_td + "'>" + i*j + "</td>";
 	}
